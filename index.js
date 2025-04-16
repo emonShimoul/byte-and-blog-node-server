4c97236c03c6cbbdad68e5dd819c478289d36418
emonShimoul <emonshimoul@gmail.com>
2025-04-16 06:59:00 +0600

    load individual blog info from db and send it to client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -84,6 +84,12 @@ async function run() {
       } catch (error) {
         res.status(500).json({ error: "Failed to fetch blogs" });
       }
+    });
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query);
+      res.send(result);
     });
     app.post("/blogs", async (req, res) => {
       const newBlog = req.body;
