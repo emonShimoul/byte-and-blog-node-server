e017414f0b915ea5728ff0d0631b956834bab45b
emonShimoul <emonshimoul@gmail.com>
2025-04-20 12:25:06 +0600

    deploy related changes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("byteAndBlogDB").collection("users");
     const blogCollection = client.db("byteAndBlogDB").collection("blogs");
@@ -244,7 +244,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
